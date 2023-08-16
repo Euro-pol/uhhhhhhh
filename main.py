a8c128f338c510e2e01e4afb4c3da77006d97ed3
Euro-pol <52547763+Euro-pol@users.noreply.github.com>
2023-08-16 14:28:01 +0200

    fire

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import os, time
 
 repo_path = input("Enter the path of the git repository: ")
-delay = int(input("Enter the delay between commits (in seconds): "))
+delay = float(input("Enter the delay between commits (in seconds): "))
 
 os.chdir(repo_path)
 
-while True:
+def commit():
     os.system("git commit --allow-empty -m 'update'")
     os.system("git push origin main")
-    time.sleep(float(delay))
+
+while True:
+    commit()
+    time.sleep(delay)
